7940ccbe789a14f0ab665704a4d710d38733566a
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-23 15:08:30 +0200

    create

--- fd-test.session.sql
+++ fd-test.session.sql
@@ -1,1 +1,27 @@
-CREATE TABLE test
+CREATE TABLE books(
+   name varchar(300),
+   author varchar(300),
+   type varchar(150),
+   pages int,
+   year date,
+   publisher varchar(256)
+);
+
+
+/*
+створіть таблицю users
+В кожного юзера має бути:
+- firstName
+- lastName
+- email
+- birthday
+- gender 
+*/
+
+CREATE TABLE users(
+    first_name varchar(256),
+    last_name varchar(256),
+    email varchar(300),
+    birthday date,
+    gender varchar(100)
+);
