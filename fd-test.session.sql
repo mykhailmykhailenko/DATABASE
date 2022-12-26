97f33771d7b9efb6e12e85aa99ce6b65f1b15e83
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-26 19:26:34 +0200

    Alter

--- fd-test.session.sql
+++ fd-test.session.sql
@@ -16,20 +16,46 @@ DROP TABLE books;
 - birthday
 - gender 
 */
-
 DROP TABLE users;
 
 CREATE TABLE users(
+    id serial PRIMARY KEY,
     first_name varchar(256) NOT NULL CHECK (first_name != ''),
     last_name varchar(256) NOT NULL CHECK (last_name != ''),
     email varchar(300) NOT NULL UNIQUE CHECK (last_name != ''),
     birthday date CHECK (birthday < current_date),
-    gender varchar(100) CHECK (last_name != ''),
-    CONSTRAINT name_pair UNIQUE(first_name, last_name)
+    gender varchar(100) CHECK (last_name != '')
 );
 
-INSERT INTO users VALUES 
-('Clark', 'Kent', 'super2@man.com', '2025-09-09', 'male');
+ALTER TABLE users
+ADD COLUMN height numeric(3,2);
+
+ALTER TABLE users
+ADD CONSTRAINT "too_high_user" CHECK (height < 4.0);
+
+
+/*
+Додати юзерам нову колонку - вагу. Вага не має бути менше 0
+Додати перевірку, що користувач народився не раніше 1990року.
+*/
+
+ALTER TABLE users
+ADD COLUMN weight numeric(5, 2) CHECK (weight > 0);
+
+ALTER TABLE users
+ADD CONSTRAINT "too_early_birthday" CHECK (birthday > '1990-01-01');
+
+
+//table_field_check
+//table_field_pkey
+
+ALTER TABLE users
+DROP CONSTRAINT "too_high_user";
+
+INSERT INTO users (first_name, last_name, email, birthday, gender) VALUES 
+('Clark', 'Kent', 'super@man.com', '2020-09-09', 'male'),
+('Iron', 'Man', 'tony@mail.com', '1990-02-02', 'male'),
+('Spider', 'MAn', 'peter@parker.com', '1992-02-02', 'male');
 
 INSERT INTO users VALUES
 (' ', '', 'tonestark2@com', '1892-02-02', 'male'),
@@ -37,8 +63,8 @@ INSERT INTO users VALUES
 ('She', '', 'tonestark3@com', '1992-02-02', 'female'),
 (' ', ' ', ' ', NULL,NULL );
 
-INSERT INTO users VALUES 
-('Clark', NULL, 4, '1990-2-2', 'male');
+INSERT INTO users  (first_name, last_name, email, birthday, gender, height) VALUES 
+('Clark', 'MArk ', 'tre@ls.s', '1990-2-2', 'male', 2.0);
 
 /*
 Створити таблицю messages
@@ -47,9 +73,7 @@ INSERT INTO users VALUES
 - created_at
 - is_read
 */
-
 DROP TABLE messages;
-
 CREATE TABLE messages (
     id serial PRIMARY KEY,
     body text NOT NULL CHECK (body != ''),
@@ -57,7 +81,6 @@ CREATE TABLE messages (
     created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp,
     is_read boolean NOT NULL DEFAULT false
 );
-
 INSERT INTO messages VALUES 
 ('helllo text', 'test user', '2029-09-09', false);
 
