200c87fab27d7fb90337fdf6e53796f776209e71
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-04 15:15:15 +0200

    tasks resolve

--- select.sql
+++ select.sql
@@ -120,3 +120,32 @@ WHERE extract("years" from age(birthday)) > 25;
 SELECT email FROM users
 WHERE gender = 'male' AND
 (extract("years" from age(birthday)) BETWEEN 18 AND 60);
+
+
+/*
+1. Отримати всіх користувачів, які народились у жовтні
+2. Отримати мейли всіх користувачів, які народились 1 листопада
+3. Всім користувачам, яким більше ніж 60 років, встановити зріст 2 метри.
+4. Всім користувачам чоловічого роду віком від 30 до 50 встановити вагу 80кг.
+*/
+
+
+----1
+SELECT * FROM users
+WHERE extract('month' from birthday) = 10;
+
+
+---2
+SELECT email FROM users
+WHERE extract('day' from birthday) = 1 AND extract('month' from birthday) = 11;
+
+---3
+UPDATE users
+SET height = 2.0
+WHERE extract('years' from age(birthday)) > 60;
+
+---4
+UPDATE users
+SET weight = 80
+WHERE (extract('years' from age(birthday)) BETWEEN 30 AND 50) 
+AND gender = 'male';
