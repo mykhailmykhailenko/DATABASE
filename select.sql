e0cc87b86ea32bd6887004069b318fd3494c7c3a
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-04 16:23:27 +0200

    aggregations

--- select.sql
+++ select.sql
@@ -212,3 +212,115 @@ WHERE char_length(fn.full_name) > 10;
 
 
 -------------------------
+
+/*
+Створити нову таблицю workers:
+- id,
+- name,
+- salary,
+- birthday
+1. Додайти робітника з ім'ям Олег, 90р.н., зп 300
+2. Додайте робітницю Ярославу, зп 1200
+3. Додайте двох нових робітників одним запитом - Сашу 85р.н., зп 1000, і Машу 95р.н., зп 900
+4. Встановити Олегу зп у 500.
+5. Робітнику з id = 4 встановити рік народження 87
+6. Всім, в кого зп > 500, врізати до 700.
+7. Робітникам з 2 по 5 встановити рік народження 99
+8. Змінити Сашу на Женю і збільшити зп до 900.
+9. Вибрати всіх робітників, чия зп > 400.
+10. Вибрати робітника з id = 4
+11. Дізнатися зп та вік Жені.
+12. Знайти робітника з ім'ям Петя.
+13. Вибрати робітників у віці 27 років або з зп > 800
+14. Вибрати робітників у віці від 25 до 28 років (вкл)
+15. Вибрати всіх робітників, що народились у вересні
+16. Видалити робітника з id = 4
+17. Видалити Олега
+18. Видалити всіх робітників старше 30 років.
+*/
+
+
+UPDATE users
+SET height = 3.0, weight = 90
+WHERE id = 2000;
+
+
+SELECT * FROM users
+WHERE id = 2000;
+
+
+---------Агрегатні функції--------
+
+SELECT max(height)
+ FROM users;
+
+
+SELECT avg(height)
+ FROM users;
+
+
+SELECT count(*)
+FROM users
+WHERE gender = 'female';
+
+---- Середня вага чоловіків
+
+SELECT avg(weight)
+FROM users
+WHERE gender = 'male';
+
+
+---------
+
+SELECT avg(weight), gender
+FROM users
+GROUP BY gender;
+
+
+
+--------
+
+SELECT avg(weight)
+FROM users
+WHERE birthday > '2000-01-01';
+
+
+-----Середня вага чоловіків старше 25 років. 
+
+SELECT avg(weight), gender
+FROM users
+WHERE extract('year' from age(birthday)) > 25
+GROUP BY gender;
+
+
+
+--1. Середня зріст всіх користувачів
+
+SELECT avg(height)
+FROM users;
+
+--2. Найбільший, найменший зріст чоловіків і жінок.
+
+SELECT max(height), min(height), gender
+FROM users
+GROUP BY gender;
+
+--3. Кількість юзерів, які народились після 1999 року.
+SELECT count(*)
+FROM users
+WHERE birthday > '1999-12-31';
+
+--4. Кількість людей з ім'ям Katie
+SELECT count(*)
+FROM users
+WHERE first_name ILIKE 'Katie';
+
+--5. Кількість людей віком від 20 до 30 років.
+SELECT count(*)
+FROM users
+WHERE extract('years' from age(birthday)) BETWEEN 20 AND 30;
+
+--6. Кількість користувачів висотою більше 1.5метри.
+SELECT count(*)
+FROM users
+WHERE height > 1.5
