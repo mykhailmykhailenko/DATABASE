c9d7e911e455affa4258a02678e348638b6fdd46
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-10 21:33:28 +0200

    task resolve

--- normalize.sql
+++ normalize.sql
@@ -261,3 +261,38 @@ PRACTICE
 4. Товар не обов'язково відгружається замовнику. Кожна відгрузка обов'язково відповідає певному товару.
  
 */
+
+CREATE TABLE products(
+    product_code serial PRIMARY KEY,
+    name varchar(300) NOT NULL CHECK(name != ''),
+    price numeric(10, 2) NOT NULL
+);
+
+CREATE TABLE customers(
+    id serial PRIMARY KEY,
+    name varchar(300) NOT NULL,
+    address text NOT NULL,
+    telephone varchar(20)
+);
+
+CREATE TABLE orders (
+    order_code serial PRIMARY KEY,
+    customer_id int REFERENCES customers(id),
+    contract_number int NOT NULL,
+    contract_date timestamp,
+    product_id int REFERENCES products(product_code),
+    quantity_plan int NOT NULL
+);
+
+CREATE TABLE shipments(
+    code serial PRIMARY KEY,
+    order_code int REFERENCES orders(order_code),
+    shipment_date timestamp NOT NULL
+);
+
+CREATE TABLE products_to_shipments (
+    product_id int REFERENCES products(product_code),
+    shipment_code int REFERENCES shipments(code),
+    quantity int NOT NULL,
+    PRIMARY KEY (product_id, shipment_code)
+);
