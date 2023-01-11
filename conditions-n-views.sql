b396623ade5b8c6c0d47dcf2aa5b90eaf06ef868
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-11 16:25:34 +0200

    Views

--- conditions-n-views.sql
+++ conditions-n-views.sql
@@ -247,3 +247,180 @@ WHERE p.id != ALL (
             SELECT product_id 
             FROM orders_to_products
 );
+
+
+--------------------------Подання (представления) - views -------------
+-- Віртуальні таблиці
+
+
+SELECT id, first_name, last_name
+FROM users;
+
+
+----- Юзери з їхньою кількістю замовлень
+SELECT u.*, count(*) AS order_count
+FROM users AS u
+JOIN orders AS o 
+ON u.id = o.customer_id
+GROUP BY u.id, u.email;
+
+
+CREATE VIEW users_with_order_count AS (SELECT u.*, count(*) AS order_count
+FROM users AS u
+JOIN orders AS o 
+ON u.id = o.customer_id
+GROUP BY u.id, u.email);
+
+
+SELECT *
+FROM users_with_order_count
+WHERE id = 292;
+
+
+UPDATE users
+SET first_name = 'Anonym'
+WHERE id = 292;
+
+
+SELECT *
+FROM users
+WHERE id = 292;
+
+
+-----
+
+
+SELECT *
+FROM users_with_order_count
+WHERE order_count > 2;
+
+
+
+
+
+---------Спрощення монстрів----
+
+/* Всі замовлення вартістю більше середнього чеку
+WITH orders_with_costs AS (
+    SELECT otp.order_id, sum(p.price*otp.quantity) AS total_amount
+    FROM orders_to_products AS otp
+    JOIN products AS p
+    ON p.id = otp.product_id
+    GROUP BY otp.order_id
+    )
+SELECT owc.*
+FROM orders_with_costs AS owc
+WHERE owc.total_amount > (SELECT avg(o_w_sum.order_sum)
+       FROM (
+            SELECT otp.order_id, sum(p.price*otp.quantity) AS order_sum
+            FROM orders_to_products AS otp
+            JOIN products AS p
+            ON p.id = otp.product_id
+            GROUP BY otp.order_id
+              ) AS o_w_sum);
+*/
+
+
+
+
+    CREATE VIEW "order_with_costs" AS 
+    ( SELECT otp.order_id, sum(p.price*otp.quantity) AS total_amount
+    FROM orders_to_products AS otp
+    JOIN products AS p
+    ON p.id = otp.product_id
+    GROUP BY otp.order_id);
+
+
+SELECT owc.*
+FROM order_with_costs AS owc
+WHERE owc.total_amount > (SELECT avg(total_amount)
+                            FROM order_with_costs);
+
+
+
+
+ /* Зробити віртуальну таблицю, яка зберігає замовлення з їхньою вартістю і кількістю замовлених моделей (product_id) */
+
+ SELECT o.*, sum(p.price*otp.quantity), count(otp.product_id)
+FROM orders AS o
+JOIN orders_to_products AS otp
+ON o.id = otp.order_id
+JOIN products AS p
+ON p.id = otp.product_id
+GROUP BY o.id;
+
+
+CREATE VIEW orders_with_sum_and_model_count AS (
+     SELECT o.*, sum(p.price*otp.quantity), count(otp.product_id)
+FROM orders AS o
+JOIN orders_to_products AS otp
+ON o.id = otp.order_id
+JOIN products AS p
+ON p.id = otp.product_id
+GROUP BY o.id
+);
+
+
+--Вивести користувачів + загальну суму всіх їхніх замовлень
+
+
+SELECT u.*, sum(owsmc.sum)
+FROM users AS u 
+JOIN orders_with_sum_and_model_count AS owsmc
+ON u.id = owsmc.customer_id
+GROUP BY u.id;
+
+
+---Зробити віртуальну таблицю з id, повним ім'ям (ім'я + прізвище), email, суму по всіх замовленнях
+
+CREATE VIEW users_fullname_with_order_sum AS (
+SELECT u.id, 
+concat(first_name, ' ', last_name) AS full_name, 
+email,
+sum(owsmc.sum) AS total_sum
+FROM users AS u 
+JOIN orders_with_sum_and_model_count AS owsmc
+ON u.id = owsmc.customer_id
+GROUP BY u.id);
+
+
+
+/*
+1. Топ-10 юзерів, які робили найдорожчі замовлення (сума по всіх замовленнях)
+2. Топ-10 юзерів, які замовляли найбільшу кількість моделей
+3. Всі замовлення, в яких було замовлено більше ніж середня кількість моделей по всіх замовленнях
+*/
+
+--1
+SELECT *
+FROM users_fullname_with_order_sum AS ofwos
+ORDER BY total_sum DESC
+LIMIT 10;
+
+--2
+SELECT u.id, u.email, sum(owsmc.count) AS model_count
+FROM users AS u
+JOIN orders_with_sum_and_model_count AS owsmc
+ON u.id = owsmc.customer_id
+GROUP BY u.id, u.email
+ORDER BY model_count DESC
+LIMIT 10;
+
+
+SELECT customer_id, sum(count) 
+FROM orders_with_sum_and_model_count AS owsmc
+GROUP BY owsmc.customer_id;
+
+
+--3
+--- Всі замовлення, в яких було замовлено більше ніж середня кількість моделей по всіх замовленнях
+
+SELECT avg(count)
+FROM orders_with_sum_and_model_count AS owsmc;
+
+SELECT *
+FROM orders_with_sum_and_model_count
+WHERE count > (
+    SELECT avg(count)
+FROM orders_with_sum_and_model_count AS owsmc
+);
